70a89cfec5ca19123a311a68b66f377ad5926179
Juan Gabriel Benitez Rinas <juangabrielrinas@gmail.com>
2024-10-12 10:49:32 -0300

    added user repository

---
+++ src/repositories/user.repository.ts
@@ -0,0 +1,15 @@
+import { UserModel, IUser } from "../models/User";
+
+export class UserRepository {
+  async create(userData: Partial<IUser>) {
+    return await UserModel.create(userData);
+  }
+
+  async findByEmail(email: string) {
+    return await UserModel.findOne({ email });
+  }
+
+  async updateStatus(userId: string, status: "active" | "inactive") {
+    return await UserModel.findByIdAndUpdate(userId, { status }, { new: true });
+  }
+}
